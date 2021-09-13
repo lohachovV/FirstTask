a9f7636a1a7f8a17b80fb89a05d41f6084936409
VLohachov <lohachov.v@gmail.com>
2021-09-13 17:27:03 +0300

    small fix

--- app/src/main/java/com/example/firsttask/mainScreen/ItemsForApps.kt
+++ app/src/main/java/com/example/firsttask/mainScreen/ItemsForApps.kt
@@ -5,7 +5,7 @@ import com.example.firsttask.mainScreen.ObjectsForDickList.Dick
     fun fillTheListOfDick() : ArrayList<Dick> {
 
         var listOfDicks : MutableList<Dick> = ArrayList()
-        for (i in 0..100){
+        for (i in 1..100){
             listOfDicks.add(Dick("Dick$i", "Pusy$i"))
         }
         return listOfDicks as ArrayList<Dick>
